5aeaba02e7bfc77c366272408755cfcf3f27ee32
Kojaehyeong <jaehyeong1586@gmail.com>
2024-01-20 20:12:58 +0900

    jpa_setting & entity

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,9 +1,21 @@
 package com.group.libraryapp.domain.user;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // tabel 생성 시 auto-increment를 설정 해주었기 때문에 IDENTITY속성
+    private Long id = null;
+
+    @Column(nullable = false, length = 20)
     private String name;
+
+    // Column어노테이션은 생략 할 수 있다. 굳이 어떠한 설정을 할 필요 없을때!
     private Integer age;
+
+    protected User() {} // JPA를 사용하기 위해서는 기본 생성자가 필요하다.
 
     public User(String name, Integer age) {
         if(name == null || name.isBlank()) {

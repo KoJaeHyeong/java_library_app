848f4db68bf3bceb6fc8f8dc75655305cd9a5890
Kojaehyeong <jaehyeong1586@gmail.com>
2024-01-21 23:29:47 +0900

    make JPA query function in Interface

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,8 +3,7 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
-import com.group.libraryapp.service.user.UserService;
-import org.springframework.jdbc.core.JdbcTemplate;
+import com.group.libraryapp.service.user.UserServiceV2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,15 +11,15 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    private final UserService userService;
+    private final UserServiceV2 userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserServiceV2 userService) {
         this.userService = userService;
     }
 
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-       userService.saveUser(request);
+        userService.saveUser(request);
     }
 
     @GetMapping("/user")
--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -15,14 +15,19 @@ public class User {
     // Column어노테이션은 생략 할 수 있다. 굳이 어떠한 설정을 할 필요 없을때!
     private Integer age;
 
-    protected User() {} // JPA를 사용하기 위해서는 기본 생성자가 필요하다.
+    protected User() {
+    } // JPA를 사용하기 위해서는 기본 생성자가 필요하다.
 
     public User(String name, Integer age) {
-        if(name == null || name.isBlank()) {
+        if (name == null || name.isBlank()) {
             throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다.", name));
         }
         this.name = name;
         this.age = age;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
@@ -31,5 +36,9 @@ public class User {
 
     public Integer getAge() {
         return age;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
     }
 }

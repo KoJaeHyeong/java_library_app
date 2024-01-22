21ea4dc5024fbf5445ecffb19dc9378401586d52
Kojaehyeong <jaehyeong1586@gmail.com>
2024-01-22 18:21:45 +0900

    book api

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,10 +20,12 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void saveUser(UserCreateRequest request) {
-        User user = userRepository.save(new User(request.getName(), request.getAge()));
+        userRepository.save(new User(request.getName(), request.getAge()));
     }
 
+    @Transactional(readOnly = true) // 저장, 변경 등의 불필요한 요소를 삭제함으로써 성능의 이점이 있음.
     public List<UserResponse> getUsers() {
         List<User> users = userRepository.findAll();
 
@@ -31,14 +34,17 @@ public class UserServiceV2 {
         return users.stream().map(UserResponse::new).collect(Collectors.toList());
     }
 
+    @Transactional // 트랜잭션으로 인해 영속성 컨텍스트가 생성된다.
     public void updateUser(UserUpdateRequest request) {
+
         User user = userRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new);
 
         user.updateName(request.getName());
-        userRepository.save(user);
+//        userRepository.save(user); 트랜잭션으로 인해서 수정된 개첵에 대해서 자동 저장 해준다.
     }
 
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
         userRepository.delete(user);
